3ab0c227adad53138ced108c141bfbe14a9d8105
Olga Shilenkova <Olga1shilenkova@gmail.com>
2022-01-06 18:15:52 -0600

    use the lightbnb database with SQL queries

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -72,10 +72,6 @@ const addUser =  function(user) {
   .catch((err) => {
     console.log(err.message);
   });
-  // const userId = Object.keys(users).length + 1;
-  // user.id = userId;
-  // users[userId] = user;
-  // return Promise.resolve(user);
 }
 exports.addUser = addUser;
 
@@ -87,7 +83,27 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+
+  const queryString = 
+  `
+    SELECT properties.*, reservations.*, AVG(rating) as average_rating
+    FROM reservations 
+    JOIN properties ON reservations.property_id= properties.id
+    JOIN property_reviews ON properties.id = property_reviews.property_id
+    WHERE reservations.guest_id = $1
+    AND reservations.end_date < now()::date
+    GROUP BY properties.id, reservations.id
+    ORDER BY reservations.start_date 
+    LIMIT $2;
+  `
+  ;
+
+   const values = [guest_id , limit];
+  
+    return pool
+    .query(queryString, values)
+    .then((result) => result.rows)
+    .catch((err) => console.log(err.message));
 }
 exports.getAllReservations = getAllReservations;
 
